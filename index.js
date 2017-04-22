0b92f20b9a7c1ada64d05c48154b86a5a32d8ab3
frank0320 <shipeng0320@gmail.com>
2017-04-22 23:47:12 +0800

    添加主机名

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.post('/upload', function (req, res) {
 
   // 指定本次上传的文件保持扩展名（默认是false，没有扩展名）
   form.keepExtensions = true
-  console.log('ip',server.address().address)
-  console.log('port',server.address().port)
+  console.log('ip', server.address().address)
+  console.log('port', server.address().port)
   form.parse(req, function (err, fields, files) {
     console.log(fields)
     console.log(files)
@@ -39,7 +39,7 @@ app.post('/upload', function (req, res) {
 })
 
 
-const server = app.listen(3000, function () {
+const server = app.listen(3000, 'localhost', function () {
   const host = server.address().address
   const port = server.address().port
   console.log("应用实例，访问地址为 http://%s:%s", host, port)
